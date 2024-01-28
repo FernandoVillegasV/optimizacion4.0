a056902dcf0f9854b8b26a1df8f98acc92d62fc2
FernandoVillegasV <157930794+FernandoVillegasV@users.noreply.github.com>
2024-01-27 21:23:32 -0500

    Update datos_conexion.py

--- Python/datos_conexion.py
+++ Python/datos_conexion.py
@@ -1,4 +1,6 @@
 smtp_address = 'smtp.gmail.com'
 smtp_port = 465
-email_address = 'fvillegas6@estudiantes.areandina.edu.co'
-email_password = 'wote wmcj cjbo qlbo'
+email_address = 'Usuario_De_Su_Correo_Electronico'
+email_password = 'Passwor_De_Su_Correo_Electronico'
+# La cuenta de correo electrónico de salida automática debe contar con una configuración específica
+# Verifique la siguiente documentación: https://www.profetolocka.com.ar/2020/12/26/micropython-usando-smtp-para-enviar-e-mails/
