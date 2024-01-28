a9e1499b72164d18817e9276c310d9ff40bcb878
FernandoVillegasV <157930794+FernandoVillegasV@users.noreply.github.com>
2024-01-27 20:16:40 -0500

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS["conexion"] = new PDO('mysql:host=localhost; dbname=id21788161_bd1000123256', 'id21788161_root1000123256', 'FernandoVillegas1212#bd');
+$GLOBALS["conexion"] = new PDO('mysql:host=localhost; dbname=Nombre_De_Su_Base_De_Datos', 'Usuario_De_Su_Base_De_Datos', 'Password_De_Su_Base_De_Datos');
 $GLOBALS["conexion"] -> exec("set names utf8");
 
 ?>
